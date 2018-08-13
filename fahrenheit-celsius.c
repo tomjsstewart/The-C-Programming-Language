17780d5ef38749f46a87a94ca322cf001da97e50
tomjsstewart@gmail.com <tomjsstewart@gmail.com>
2018-08-13 21:31:04 +0100

    Right justify numbers

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -15,7 +15,7 @@ main()
     while(fahr <= upper)
     {
         celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d\t%6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
