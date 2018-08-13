79727821dcd7956c62591465d344e25a6e74a370
tomjsstewart <tomjsstewart@gmail.com>
2018-08-13 21:25:11 +0100

    Fahrenheit to Celsius converter

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -14,7 +14,8 @@ main()
     fahr = lower;
     while(fahr <= upper)
     {
-        celsius = 5 * (fahr-32) / 9;        printf("%d\t%d\n", fahr, celsius);
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
