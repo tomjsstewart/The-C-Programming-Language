88bf4e29efe1fd92fb932dfa21142bab75ed6ad6
tomjsstewart@gmail.com <tomjsstewart@gmail.com>
2018-08-13 22:30:44 +0100

    Character counting - for loop

--- character-counting.c
+++ character-counting.c
@@ -4,10 +4,7 @@ main()
 {
     long nc;
 
-    nc = 0;
-    while(getchar() != EOF)
-    {
-        ++nc;
-    }
-    printf("%ld\n", nc);
+    for(nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
